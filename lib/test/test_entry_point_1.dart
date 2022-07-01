7497dd923ff72ac62f0afe19b636685ea0344df0
sstkachev <sstkachev@gmail.com>
2022-07-01 22:26:14 +0300

    simple_layout_authorisation _screen_point_1

--- lib/test/test_entry_point_1.dart
+++ lib/test/test_entry_point_1.dart
@@ -21,8 +21,38 @@ class TestEntryPoint1 extends StatelessWidget {
       appBar: AppBar(
         title: const Text('VK TEST_ENTRY_POINT_1'),
       ),
-      body: const Center(
-        child: Text('TEST_ENTRY_POINT_1'),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
+        child: Column(
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Login'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(), hintText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: () {}, child: const Text('Войти')),
+                const SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text('Авторизоваться')),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
